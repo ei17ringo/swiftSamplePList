a7b5f9d4af5c0615355d52921537b111b5853e66
ei17ringo <ei17ringo@gmail.com>
2016-02-11 13:25:00 +0800

    Initial Commit

---
+++ samplePList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  samplePList
+//
+//  Created by Eriko Ichinohe on 2016/02/11.
+//  Copyright © 2016年 Eriko Ichinohe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
